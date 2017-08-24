231a59c610795245f6772f3ac28158a5e7daa8a5
Silver Ibenye <silver.ibenye@forrent.com>
2017-08-24 11:03:22 -0400

    fix homepage height

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Home')
 
 @section('body')
-        <div id="home" class="container full-height">
+        <div id="home" class="container">
 
 
             <div class="content">
